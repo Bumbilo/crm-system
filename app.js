2250ec80a71dc5f404cdb28cec8a2b4d78f0ac2c
Bumbilo <dimagolyak@gmail.com>
2018-08-02 13:49:06 +0300

    added passport for auth

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
@@ -21,6 +22,9 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
+app.use(passport.initialize());
+require('./middleware/passport')(passport)
 
 app.use(bodyParser.json());
 app.use(cors());
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcryptjs');
-const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 const keys = require('../config/keys');
+const User = require('../models/User');
 
 module.exports.login = async (req, res) => {
     const candidate = await User.findOne({
