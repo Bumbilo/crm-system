dd609ca747abb0693174a10b2c9bd51acb6fbfb6
dima.golyak <dimagolyak@gmail.com>
2018-08-06 13:37:20 +0300

    added controlers for routes category and position

--- controllers/category.js
+++ controllers/category.js
@@ -1,19 +1,55 @@
-module.exports.getAll = (req, res) => {
-    res.json({message: 'Categories'});
+const Category = require('../models/Category');
+const Position = require('../models/Position');
+const errorHandler = require('../utils/errorHandler');
+
+module.exports.getAll = async (req, res) => {
+    try {
+        const categories = await Category.find({
+            user: req.user.id
+        })
+        res.status(200).json(categories);
+    } catch (error) {
+        errorHandler(res, error);
+    }
 }
 
-module.exports.getById = (req, res) => {
-
+module.exports.getById = async (req, res) => {
+    try {
+        const category = await Category.findById(req.pramas.id)
+        res.status(200).json(category);
+    } catch (error) {
+        errorHandler(res, error);
+    }
 }
 
-module.exports.remove = (req, res) => {
-
+module.exports.remove = async (req, res) => {
+    try {
+        await Category.remove({
+            _id: req.pramas.id
+        });
+        await Position.remove({
+            category: req.pramas.id
+        });
+        res.status(200).json({
+            message: 'Скатегория удалена'
+        })
+    } catch (error) {
+        errorHandler(res, error);
+    }
 }
 
 module.exports.create = (req, res) => {
+    try {
 
+    } catch (error) {
+        errorHandler(res, error);
+    }
 }
 
 module.exports.update = (req, res) => {
+    try {
 
+    } catch (error) {
+        errorHandler(res, error);
+    }
 }
